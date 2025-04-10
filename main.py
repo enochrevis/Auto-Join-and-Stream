afde345065c3d655e318d49920197baad7c2cf76
enochrevis <enochrevis@gmail.com>
2025-04-09 23:14:15 -0700

    Made certain functionality modular, added checks to see if you are in a vc without streaming, and added checks to join a populated vc

--- main.py
+++ main.py
@@ -6,22 +6,59 @@ def click_picture(picture):
     pictureX, pictureY = pyautogui.locateCenterOnScreen(picture)
     pyautogui.click(pictureX, pictureY)
 
+def begin_streaming():
+        try:
+            click_picture('stream-icon.png')
+            click_picture('go-live-button.png')
+            click_picture('grant-access-button.png')
+        except:
+            print("Failed to start stream.")
+
+def check_channel_capacity(channel):
+    try:
+        pyautogui.locateOnScreen(channel)
+        return 1
+    except:
+        return 0
+
 while True:
     try:
         pyautogui.locateOnScreen('streaming.png')
         print("Currently streaming")
+
     except:
-        print("Currently not streaming, joining voice chat...")
+        print("Currently not streaming, check to see if already in voice chat...")
 
         try:
-            click_picture('secret-test-channel.png')
-
-            click_picture('stream-icon.png')
-
-            click_picture('go-live-button.png')
-
-            click_picture('grant-access-button.png')
-            
+            pyautogui.locateOnScreen('not-streaming.png')
+            print("Currently in voice chat, starting stream...")
+            begin_streaming()
+        
         except:
-            print("Failed to click picture, retrying...")
+            print("Currently not in voice chat, joining...")
+
+            try:
+                activeChannels = check_channel_capacity('albedo-vator-channel-capacity-0.png') << 2
+                activeChannels += check_channel_capacity('wanmin-restaurant-channel-capacity-0.png') << 1
+                activeChannels += check_channel_capacity('jade-chamber-channel-capacity-0.png')
+                allOnesMask = 0b111
+
+                if (activeChannels & allOnesMask):
+                    click_picture('albedo-vator.png')
+                    begin_streaming()
+                elif (activeChannels ^ (1 << 2)):
+                    click_picture('albedo-vator.png')
+                    begin_streaming()
+                elif (activeChannels ^ (1 << 1)):
+                    click_picture('wanmin-restaurant.png')
+                    begin_streaming()
+                elif (activeChannels ^ 1):
+                    click_picture('jade-chamber.png')
+                    begin_streaming()
+                else:
+                    click_picture('albedo-vator.png')
+                    begin_streaming()
+
+            except:
+                print("Failed to click picture, retrying...")
 
